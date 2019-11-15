f05affc8642a06d25e0d64f11828f237d31e8eb2
Eddy Tyannikov <etyannikov@gmail.com>
2019-11-15 19:20:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -29,13 +29,14 @@ class Dog
   
   def save
     if self.id
-      
+      sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+      DB[:conn].execute(sql, self.name, self.breed, self.id)
     else
-    sql = <<-SQL 
-    INSERT INTO dogs (name, breed)
-    VALUES (? ,?)
-    SQL
-    
-    DB[:conn].execute(sql, self.name, self.breed)
+      sql = <<-SQL 
+      INSERT INTO dogs (name, breed)
+      VALUES (? ,?)
+      SQL
+      
+      DB[:conn].execute(sql, self.name, self.breed)
   end
 end
