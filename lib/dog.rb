d42ee963f9432089814fed3d37a14f2fa8746a5c
Eddy Tyannikov <etyannikov@gmail.com>
2019-11-15 19:06:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,8 +2,14 @@ class Dog
   attr_accessor :id,:name, :breed
   
   def initialize(hash)
+    @id = hash[:id]
     @name = hash[:name]
     @breed = hash[:breed]
   end
   
+  def self.create_table
+    sql = <<-SQL 
+    CREATE TABLE IF NOT EXISTS dogs
+    SQL
+  end
 end
